80e3e3302d98bdb43d0a8fa944e3e70b9a95e015
Daniel Donateli <daniel.donateli@appmax.com.br>
2025-08-25 12:51:42 -0300

    Add general plugin and setup geiser keybinds for evil

--- config/evil-config.el
+++ config/evil-config.el
@@ -8,12 +8,55 @@
    :config
    (evil-mode 1))
 
+(use-package general
+  :config
+  (general-evil-setup t))
+
 (use-package evil-collection
   :after evil
   :ensure t
   :config
   (evil-collection-init))
 
-(evil-set-leader '(motion normal) (kbd "SPC"))
+(use-package evil-cleverparens
+  :hook ((emacs-lisp-mode
+          clojure-mode
+          scheme-mode
+          lisp-mode) . evil-cleverparens-mode)
+  :config
+  (setq evil-cleverparens-use-additional-movement-keys t)
+  (add-hook 'geiser-repl-mode-hook #'evil-cleverparens-mode))
+
+;; (evil-set-leader '(motion normal) (kbd "SPC"))
+
+;; general keybinds
+(general-create-definer my/leader
+                        :states '(normal visual)
+                        :prefix "SPC")
+
+(general-create-definer my/local-leader
+                        :states '(normal visual)
+                        :prefix "SPC m")
+
+(general-create-definer my/local-leader-alt
+                        :states '(normal visual)
+                        :prefix ",")
+
+;; Define Geiser keybindings
+(setq my/geiser-bindings
+      '("e"  '(:ignore t :which-key "eval")
+        "ee" '(geiser-eval-last-sexp :which-key "eval last sexp")
+        "ed" '(geiser-eval-definition :which-key "eval definition")
+        "eb" '(geiser-eval-buffer :which-key "eval buffer")
+        "er" '(geiser-eval-region :which-key "eval region")
+
+        "h"  '(:ignore t :which-key "help")
+        "hd" '(geiser-doc-symbol-at-point :which-key "doc for symbol")
+        "ha" '(geiser-doc-apropos :which-key "apropos")
+        "hm" '(geiser-doc-look-up-manual :which-key "manual lookup")))
+
+;; Apply to both leaders
+(dolist (definer '(my/local-leader my/local-leader-alt))
+  (eval `(,definer :keymaps 'scheme-mode-map ,@my/geiser-bindings)))
 
 (provide 'evil-config)
